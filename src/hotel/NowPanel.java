f26bda10edf10aa495fa8d6e86e5b9fccc7b3e3c
SIST108\user <shaqbari@gmail.com>
2017-04-13 19:29:13 +0900

    nowpanel에 url추가해보자

--- src/hotel/NowPanel.java
+++ src/hotel/NowPanel.java
@@ -1,14 +1,13 @@
 /*DBProject 2 참고*/
 package hotel;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Image;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -16,7 +15,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 
@@ -92,14 +90,18 @@ public class NowPanel extends JPanel{
 				String number = Integer.toString(room.getRoom_option_id());
 				//ㄴ임시로 room_option_id 넣음. 추후에 join 통해 rooom_number 구해와야 함.
 				String name = room.getRoom_option_name();
+						
+				/*---------------------------선생님께 질문-----------------------------------------------*/
+				URL url=this.getClass().getResource(room.getRoom_option_img());
 				Image img = ImageIO.read(new File(path+room.getRoom_option_img()));
+				/*-----------------------------------------------------------------------------*/
 				
 				Room_Item item = new Room_Item(number, name, img);
 				add(item);
 				
 			} catch (IOException e) {
 				e.printStackTrace();
-			}
+			} 
 		}
 	}
 	
