06366914db34ba9867d8f05e0a99bae8bfb24448
SIST108\user <shaqbari@gmail.com>
2017-04-14 13:58:30 +0900

    폰트수정, 이미지 수정

--- src/hotel/MyButton.java
+++ src/hotel/MyButton.java
@@ -2,6 +2,7 @@ package hotel;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.URL;
@@ -11,49 +12,51 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class MyButton extends JPanel implements ActionListener{
-	HotelMain main;	
+public class MyButton extends JPanel implements ActionListener {
+	HotelMain main;
 	JButton bt;
 	JLabel la;
 	ImageIcon icon;
-	boolean flag=true;
+	boolean flag = true;
 	URL url;
-	
+
 	public MyButton(HotelMain main, JButton bt, URL url, String menu) {
-		this.main=main;
-		this.bt=bt;		
-		icon=new ImageIcon(url);
-		la=new JLabel(menu);
-		
+		this.main = main;
+		this.bt = bt;
+		icon = new ImageIcon(url);
+		la = new JLabel(menu);
+
+		la.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 20));
+
 		setPreferredSize(new Dimension(120, 150));
-		bt.setPreferredSize(new Dimension(100, 100));		
+		bt.setPreferredSize(new Dimension(100, 100));
 		bt.setIcon(icon);
-		
+
 		bt.setBorderPainted(false);
 		bt.setContentAreaFilled(false);
 		bt.setFocusPainted(false);
 		bt.setOpaque(false);
-		
+
 		bt.addActionListener(this);
-		
-		add(bt);		
+
+		add(bt);
 		add(la);
 	}
 
 	public void actionPerformed(ActionEvent e) {
 		if (flag) {
-			flag=!flag;
+			flag = !flag;
 			for (int i = 0; i < main.myButtons.length; i++) {
 				this.setBackground(Color.LIGHT_GRAY);
-				if(main.myButtons[i]!=this){
+				if (main.myButtons[i] != this) {
 					main.myButtons[i].setBackground(null);
-					main.myButtons[i].flag=true;					
+					main.myButtons[i].flag = true;
 				}
 			}
-		}else {
-			flag=!flag;
+		} else {
+			flag = !flag;
 			this.setBackground(null);
 		}
-	}	
-	
+	}
+
 }
