20c63e3b6ad395906dfb7cf83e9040f265ea402c
dagiyeja@gmail.com <dagiyeja@gmail.com>
2017-04-14 18:00:20 +0900

    예약관리 테이블 초안

---
+++ src/hotel/ResvModel.java
@@ -0,0 +1,135 @@
+package hotel;
+ 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Calendar;
+import java.util.Vector;
+ 
+import javax.swing.table.AbstractTableModel;
+ 
+public class ResvModel extends AbstractTableModel{
+	Vector<String> columnName=new Vector<String>(); //컬럼명:날짜 
+	Vector <Vector> data=new Vector<Vector>(); //호수
+	HotelMain main;
+	Connection con;
+	Calendar cal;
+	int yy;
+	int mm;
+	int dd;
+	int lastDay;
+	
+	public ResvModel(Connection con,Calendar cal) {
+		this.con=con; 
+		//열에 날짜 데이터가 들어옴
+		this.cal=cal;
+		yy=cal.get(Calendar.YEAR);
+		mm=cal.get(Calendar.MONTH);
+		
+		
+		lastDay=cal.get(Calendar.DATE);
+		int num=0;
+		
+		columnName.add("날짜");
+		
+		for(int i=num; i<lastDay; i++){
+			num++;
+			columnName.add(Integer.toString(num));
+		}
+		
+		getList();
+		
+	}
+	
+	
+	
+	/*-------------------------------------------------------
+	 * Room 테이블의 room_number를 첫번째 열에 가져옴 
+	 *-------------------------------------------------------*/
+	 
+	public void getList(){
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+
+		/*
+		String sql="select resv_id, hotel_user_id, room_number, to_char(resv_time, 'yy-mm-dd') as resv_time from resv where to_char(resv_time, 'yyyy')=?";
+		sql+=" and to_char(resv_time,'mm')=?";
+		*/
+		String sql="select resv_id, hotel_user_id, room_number, to_char(resv_time, 'yy-mm-dd') as resv_time from resv where to_char(resv_time, 'yyyy')='"+yy+"'";
+		sql+=" and to_char(resv_time,'mm')='"+DateUtil.getDateString(Integer.toString(mm))+"'";
+
+		System.out.println(sql);
+		
+		try {
+			pstmt=con.prepareStatement(sql);
+			//pstmt.setString(1, Integer.toString(yy));
+			//pstmt.setString(2, DateUtil.getDateString(Integer.toString(mm)));
+			
+			rs=pstmt.executeQuery();
+			
+			while(rs.next()){
+				Vector vec=new Vector();
+				
+				//각 월별 날짜수에 따른 반복문 
+				vec.add(rs.getInt("room_number"));
+				
+				for(int i=1;i<=lastDay;i++){
+					//포문의 i가 날짜이므로, 이 날짜와 rs의 날짜가 일치하면 빨간 사각형 넣자!!
+					int date=Integer.parseInt(rs.getString("resv_time").split("-")[2]);
+					
+					if(i==date){
+						vec.addElement("O");
+						//System.out.println(i+"일에 예약발견");
+					}else{
+						vec.addElement("X");
+					}
+					//System.out.print(i+"일,");
+				}
+				System.out.println("");
+				data.add(vec);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally{
+			if(rs!=null){
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(pstmt!=null){
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+			
+	}
+	
+	public int getColumnCount() {
+		
+		return columnName.size();
+	}
+ 
+	
+	public int getRowCount() {
+		
+		return data.size();
+		
+	}
+	
+	public String getColumnName(int col) {
+		return columnName.get(col);
+	}
+	
+	
+ 
+	public Object getValueAt(int row, int col) {
+		return data.get(row).get(col);
+	}
+ 
+}
--- src/hotel/ResvPanel.java
+++ src/hotel/ResvPanel.java
@@ -1,22 +1,109 @@
 package hotel;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.Connection;
+import java.util.Calendar;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
-public class ResvPanel extends JPanel{
+public class ResvPanel extends JPanel implements ActionListener{
 	HotelMain main;
 	Connection con;
+	JPanel p_north, p_center;
+	JButton b_prev, b_next;
+	JLabel la_date;
+	JScrollPane scroll;
+	JTable table;
+	Calendar cal=Calendar.getInstance();
+	ResvModel resvModel;
+	
+	//현재 날짜 받아오는 변수
+	int yy;
+	int mm;
+	int dd;
 	
 	public ResvPanel(HotelMain main) {
 		this.main=main;
 		con=main.con;
 		
-		setBackground(Color.CYAN);
+		yy=cal.get(cal.YEAR);
+		mm=cal.get(cal.MONTH);
+		dd=cal.get(cal.DAY_OF_MONTH);
+		System.out.println("실제 월값은 "+mm);
+		this.setLayout(new BorderLayout());
+		p_north=new JPanel();
+		p_center=new JPanel();
+		b_prev=new JButton("◀");
+		la_date=new JLabel(yy+"-"+(mm+1)+"-"+dd);
+		b_next=new JButton("▶");
+		table=new JTable();
+		scroll=new JScrollPane(table);
 		
+		table.setPreferredScrollableViewportSize(new Dimension(1000, 750));
+		
+		p_north.add(b_prev);
+		p_north.add(la_date);
+		p_north.add(b_next);
+		p_center.add(scroll);
+		add(p_north, BorderLayout.NORTH);
+		add(p_center,BorderLayout.CENTER);
+		
+		b_prev.addActionListener(this);
+		b_next.addActionListener(this);
+		
+		setBackground(Color.CYAN);
 		setPreferredSize(new Dimension(1100, 900));
 		setVisible(false);			
+	}
+
+	public void prev(){
+		la_date.setText(yy+"-"+(mm-1)+"-"+dd);
+		mm--;
+		
+		if(mm<0){
+			mm=11;
+			yy--;
+		}
+		p_center.updateUI();
+
+		cal.set(yy, mm, 0);
+		ResvModel rm=new ResvModel(con,cal);
+		table.setModel(rm);
+		table.updateUI();
+		
+	}
+	
+	public void next(){
+		la_date.setText(yy+"-"+(mm+1)+"-"+dd);
+		
+		mm++;
+		if(mm>11){
+			mm=0;
+			yy++;
+		}
+		ResvModel rm=new ResvModel(con,cal);
+		p_center.updateUI();
+		cal.set(yy, mm, 0);
+		table.setModel(rm);
+		table.updateUI();		
+	}
+	
+	
+	public void actionPerformed(ActionEvent e) {
+		Object obj=e.getSource();
+		if(obj==b_prev){
+			prev();
+		}else if(obj==b_next){
+			next();
+		}
 	}
 }
